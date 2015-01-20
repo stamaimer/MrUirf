2214c0d52d8c03c248fc2c52d0f35faaad1604a2
stamaimer <stamaimer@gmail.com>
2015-01-20 21:00:49 +0800

    modify oauth.py

--- twitter/src/oauth.py
+++ twitter/src/oauth.py
@@ -1,23 +1,34 @@
+import urllib
 import base64
 import requests
+
+def urlencode(str):
+
+    return urllib.quote(str, '')
+
+def b64encode(str):
+
+    return base64.b64encode(str)
 
 BEARER_TOKEN_URL = "https://api.twitter.com/oauth2/token"
 
 def get_bearer_token(consumer_key, consumer_secret):
 
-    bearer_token_credentials    = consumer_key + ":" +consumer_secret
+    bearer_token_credentials     = ':'.join([urlencode(consumer_key), urlencode(consumer_secret)])
 
-    bearer_token_credentials_b64= base64.b64encode(bearer_token_credentials)
+    bearer_token_credentials_b64 = b64encode(bearer_token_credentials)
 
-    headers, payload            = {}, {}
+    headers, payload             = {}, {}
 
-    headers['Authorization']    = "Basic " + bearer_token_credentials_b64
+    headers['Authorization']     = ' '.join(['Basic', bearer_token_credentials_b64])
 
-    headers['Content-Type']     = "application/x-www-form-urlencoded;\
-                                   charset=UTF-8"
-    payload['grant_type']       = "client_credentials"
+    headers['Content-Type']      = 'application/x-www-form-urlencoded;charset=UTF-8'
+
+    payload['grant_type']        = "client_credentials"
 
     response = requests.post(BEARER_TOKEN_URL, data=payload, headers=headers)
+
+    print response.request.data
 
     bearer_token = response.json()['access_token']
 
