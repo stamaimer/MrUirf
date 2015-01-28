dd798845c5076be5e692ca98ac7dbeac7f695b74
stamaimer <stamaimer@gmail.com>
2015-01-28 15:54:35 +0800

    add gen network

--- gen_network.py
+++ gen_network.py
@@ -1,1 +1,45 @@
+import requests
 import networkx
+import argparse
+import json
+import time
+
+ids = []
+
+nodes = []
+links = []
+
+def get_followers(id):
+
+    pass
+
+def get_following(id):
+
+    pass 
+
+if __name__ == '__main__':
+
+    argument_parser = argparse.ArgumentParser(description='')
+
+    argument_parser.add_argument('-i', 'id', help='')
+
+    argument_parser.add_argument('-d', 'depth', help='')
+
+    args = argument_parser.parse_args()
+
+    id = args.id
+
+    max_depth = args.depth
+
+    ids.append({'id':id, 'depth':0})
+
+    for id in ids:
+
+        if id['depth'] > max_depth:
+
+            pass
+
+        else:
+
+            get_followers(id)
+            get_following(id)
