01e3f7d9a2783c123bf5189c4372186bdac85b2b
stamaimer <stamaimer@gmail.com>
2015-02-10 22:41:03 -0500

    genrate random adj matrix with numpy

--- cal_similarity_martrix.py
+++ cal_similarity_martrix.py
@@ -5,12 +5,22 @@ if __name__ == '__main__':
 
     argument_parser = argparse.ArgumentParser(description='')
 
-    argument_parser.add_argument('path2adjmatg', help='')
-    argument_parser.add_argument('path2adjmatt', help='')
+    argument_parser.add_argument('-p2g', '--path2adjmatg', help='')
+    argument_parser.add_argument('-p2t', '--path2adjmatt', help='')
 
     args = argument_parser.parse_args()
 
-    path2adjmatg = args.path2adjmatg
-    path2adjmatt = args.path2adjmatt
+    #读入两个方阵
 
+    if path2adjmatg and path2adjmatt:
 
+        path2adjmatg = args.path2adjmatg
+        path2adjmatt = args.path2adjmatt
+
+    #随机生成两个方阵
+
+    else:
+
+        g = numpy.reshape(numpy.random.random_integers(0, 1, size=100), (10, 10))
+
+        t = numpy.reshape(numpy.random.random_integers(0, 1, size=100), (10, 10))
