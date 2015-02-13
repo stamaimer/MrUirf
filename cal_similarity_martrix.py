2ac073d40bb6f120355b33c500b321f34f9ed935
stamaimer <stamaimer@gmail.com>
2015-02-13 20:07:41 +0800

    implement iterate one

--- cal_similarity_martrix.py
+++ cal_similarity_martrix.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy
+import random
 import argparse
 
 if __name__ == '__main__':
@@ -14,7 +15,7 @@ if __name__ == '__main__':
 
     #读入两个方阵
 
-    if path2adjmatg and path2adjmatt:
+    if args.path2adjmatg and args.path2adjmatt:
 
         path2adjmatg = args.path2adjmatg
         path2adjmatt = args.path2adjmatt
@@ -23,12 +24,22 @@ if __name__ == '__main__':
 
     else:
 
-        g = numpy.reshape(numpy.random.random_integers(0, 1, size=100), (10, 10))
+        nodes4g = random.randint(1, 10)
 
-        t = numpy.reshape(numpy.random.random_integers(0, 1, size=100), (10, 10))
+        nodes4t = random.randint(1, 10)
 
-        s = numpy.ones((10, 10))
+        g = numpy.reshape(numpy.random.random_integers(0, 1, size=nodes4g*nodes4g), (nodes4g, nodes4g))
+
+        t = numpy.reshape(numpy.random.random_integers(0, 1, size=nodes4t*nodes4t), (nodes4t, nodes4t))
+
+        s = numpy.ones((nodes4t, nodes4g), dtype=numpy.int64)
 
         print g
         print t
         print s
+
+        tmp = numpy.dot(numpy.dot(t, s), g.transpose()) + numpy.dot(numpy.dot(t.transpose(), s), g)
+
+        tmp = tmp / numpy.linalg.norm(tmp)
+
+        print tmp
