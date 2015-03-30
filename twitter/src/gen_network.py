be83a8d674b210fc3508928c1fbce0fb58010c09
stamaimer <stamaimer@gmail.com>
2015-03-30 11:19:48 +0800

    rm get bearer token from gen

--- twitter/src/gen_network.py
+++ twitter/src/gen_network.py
@@ -1,8 +1,7 @@
 import json
 import time
 import numpy
-import urllib
-import base64
+import oauth
 import requests
 import networkx
 import argparse
@@ -17,11 +16,10 @@ links = []
 CONSUMER_KEY = "6s35FXsv4jD2ar0ZlDYjnt7jZ"
 CONSUMER_SECRET = "oFAlNZr6JGHwCdYGrYNfS3plUSdxg8UlEP2RtiKg59uSYahWRk"
 
-BEARER_TOKEN_URL = "https://api.twitter.com/oauth2/token"
 FOLLOWING_URL = "https://api.twitter.com/1.1/friends/list.json"
 FOLLOWERS_URL = "https://api.twitter.com/1.1/followers/list.json"
 
-headers = {"Authorization" : 'Bearer ' + get_bearer_token()}
+headers = {"Authorization" : 'Bearer ' + oauth.get_bearer_token()}
 
 ratelimit_remaining = '30'
 ratelimit_reset = time.time()
@@ -83,32 +81,6 @@ def urlencode(str):
 def b64encode(str):
 
     return base64.b64encode(str)
-
-def get_bearer_token(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET):
-
-    bearer_token_credentials = ':'.join([urlencode(consumer_key), urlencode(consumer_secret)])
-
-    b64encoded_bearer_token_credentials = b64encode(bearer_token_credentials)
-
-    headers, payload = {}, {}
-
-    headers["Authorization"] = "Basic " + b64encoded_bearer_token_credentials
-
-    headers["Content-Type"] = "application/x-www-form-urlencoded;charset=UTF-8"
-
-    payload["grant_type"] = "client_credentials"
-
-    response = requests.post(BEARER_TOKEN_URL, headers=headers, data=payload)
-
-    if 200 == response.status_code:
-
-        return response.json()["access_token"]
-
-    else:
-
-        print response.status_code, response.json()
-
-        return None
 
 def find_by_name(name):
 
--- twitter/src/oauth.py
+++ twitter/src/oauth.py
@@ -2,32 +2,30 @@ import urllib
 import base64
 import requests
 
-def urlencode(str):
-
-    return urllib.quote(str, '')
-
-def b64encode(str):
-
-    return base64.b64encode(str)
-
 BEARER_TOKEN_URL = "https://api.twitter.com/oauth2/token"
 
 def get_bearer_token(consumer_key, consumer_secret):
 
-    bearer_token_credentials     = ':'.join([urlencode(consumer_key), urlencode(consumer_secret)])
+    bearer_token_credentials = ':'.join([urlencode(consumer_key), urlencode(consumer_secret)])
 
-    bearer_token_credentials_b64 = b64encode(bearer_token_credentials)
+    b64encoded_bearer_token_credentials = b64encode(bearer_token_credentials)
 
-    headers, payload             = {}, {}
+    headers, payload = {}, {}
 
-    headers['Authorization']     = ' '.join(['Basic', bearer_token_credentials_b64])
+    headers["Authorization"] = "Basic " + b64encoded_bearer_token_credentials
 
-    headers['Content-Type']      = 'application/x-www-form-urlencoded;charset=UTF-8'
+    headers["Content-Type"] = "application/x-www-form-urlencoded;charset=UTF-8"
 
-    payload['grant_type']        = "client_credentials"
+    payload["grant_type"] = "client_credentials"
 
-    response = requests.post(BEARER_TOKEN_URL, data=payload, headers=headers)
+    response = requests.post(BEARER_TOKEN_URL, headers=headers, data=payload)
 
-    bearer_token = response.json()['access_token']
+    if 200 == response.status_code:
 
-    return bearer_token
+        return response.json()["access_token"]
+
+    else:
+
+        print response.status_code, response.json()
+
+        return None
