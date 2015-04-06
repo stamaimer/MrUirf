9be996a6b0eb62aeb16890a7fad9ce34e4b7cd36
stamaimer <stamaimer@gmail.com>
2015-04-06 11:13:49 +0800

    change layout

---
+++ facebook/gen_network.py
@@ -0,0 +1,144 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+start_url = "http://m.facebook.com"
+user_email= "wendyfrank@126.com"
+user_pass = "wendyfrank"
+names = {}
+persons = {}
+graph = {"nodes": [], "links": []}
+
+def login(driver):
+
+    driver.get(start_url)
+    driver.find_element_by_name('email').send_keys(user_email)
+    driver.find_element_by_name('pass').send_keys(user_pass)
+    driver.find_element_by_name('login').click()
+
+    print "logged in."
+
+def set_start_peer(driver, start_peer):
+    global presons
+    global names
+    global graph
+
+    name = start_peer['name']
+    link = start_peer['link']
+    names[name.lower()] = '0'
+    graph['nodes'].append({'group':'0', 'name':name})
+    persons['0'] = {'group':'0', 'name':name, 'index':'0', 'father_index':'0', 
+                    'link': link}
+
+def exec_peer(driver, person):
+
+    print "-" * 50
+    print "name   : %s" % person['name']
+    print "link   : %s" % person['link']
+    print "group  : %s" % person['group']
+    print "index  : %s" % perosn['index']
+    print "father : %s" % person['father_index']
+
+    driver.get(person['link'])
+    first_cover_xpath = '/html/body/div/div/div[2]/div/div/div[2]'
+    first_friends_xpath_template ='/html/body/div/div/div[2]/div/div/div[2]/div[%s]/table/tbody/tr/td[2]/a'
+    more_cover_xpath = '/html/body/div/div/div[2]/div/div/div[1]'
+    more_friends_xpath_template = 'html/body/div/div/div[2]/div/div/div[1]/div[%s]/table/tbody/tr/td[2]/a'
+
+    # get_friend_list
+    driver.find_element_by_xpath('/html/body/div/div/div[2]/div/div/div[1]/div[4]/a[2]').click()
+
+    # get first friend page
+    scan_friends(driver, first_cover_xpath, first_friends_xpath_template, person)
+
+    # get more
+    while True:
+        try:
+            more_button = driver.find_element_by_id('m_more_friends')
+            more_button.find_element_by_tag_name('a').click()
+            scan_friends(driver,more_cover_xpath,more_friends_xpath_template,person)
+        except:
+            break
+
+    print "finished."
+
+def scan_friends(driver, cover_xpath, friend_xpath_template, father):
+    global names
+    global persons
+    global graph
+
+    cover = driver.find_element_by_xpath(cover_xpath)
+    page_friends = cover.find_elements_by_tag_name('table')
+
+    for i in range(1, len(page_friends)+1):
+
+        friend = driver.find_element_by_xpath(friend_xpath_template % i)
+        name = friend.get_attribute('innerHTML').lower()
+        if name == persons[father['father_index']]['name'].lower():
+            pass
+        elif name in names.keys():
+            link1, link2 = {}, {}
+            link1['source'] = father['index']
+            link1['target'] = names[name]
+            link2['source'] = names[name]
+            link2['target'] = father['index']
+            graph['links'].append(link1)
+            graph['links'].append(link2)
+        else:
+            person = {}
+            person['name'] = friend.get_attribute('innerHTML')
+            person['link'] = friend.get_attribute('href')
+            person['group']= str(int(father['group'])+1)
+            person['index']= str(len(graph['nodes']))
+            person['father_index'] = father['index']
+            persons[person['index']] = person
+
+            names[name] = person['index']
+
+            node = {}
+            link1, link2 = {}, {}
+            node['group'] = person['group']
+            node['name']  = person['name']
+            link1['source'] = father['index']
+            link1['target'] = person['index']
+            link2['source'] = person['index']
+            link2['target'] = father['index']
+            graph['nodes'].append(node)
+            graph['links'].append(link1)
+            graph['links'].append(link2)
+
+def default_start():
+    name='Aoi Teshima'
+    link="http://m.facebook.com/profile.php?id=100003739153500&fref=fr_tab&refid=17"
+    return {'name':name, 'link':link}
+
+def gen_network(depth = 2, start_peer = default_start()):
+    global persons
+
+    driver = webdriver.Firefox()        # open browser
+    login(driver)                       # login
+    set_start_peer(driver, start_peer)  # set the seed user
+
+    for group in range(depth):
+        for p in [persons[k] for k in persons.keys() if persons[k]['group'] == str(group)]:
+            try:
+                exec_peer(driver, p)
+            except:
+                pass
+
+    driver.close()
+    return graph
+
+if __name__ == "__main__":
+    name = u'\u738b\u5e05'
+    link = "https://m.facebook.com/stamaimer?refid=46&sld=eyJzZWFyY2hfc2lkIjoiNTNlYmZiMDQ4YTExNTM2N2E1OWJmMzE3N2U0NmJiOTciLCJxdWVyeSI6InN0YW1haW1lciIsInNlYXJjaF90eXBlIjoiU2VhcmNoIiwic2VxdWVuY2VfaWQiOjg4NDMyNTE3NSwicGFnZV9udW1iZXIiOjEsImZpbHRlcl90eXBlIjoiU2VhcmNoIiwiZW50X2lkIjoxMDAwMDUwODAxNzM2NDYsInBvc2l0aW9uIjowLCJyZXN1bHRfdHlwZSI6MjA0OH0%3D&fref=search"
+
+#    f_network = gen_network(2, {'name':name, 'link':link})
+#    f = file('graph2.json', 'w+')
+#    json.dump(f_network, f)
+#    f.close()
+
+    f_network = gen_network(3, {'name':name, 'link':link})
+    f = file('graph3.json', 'w+')
+    json.dump(f_network, f)
+    f.close()
---
+++ github/crawl.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import argparse
+import pymongo
+import json
+import time
+
+
+
+client_id = '1c6409e7a4219e6dea66'
+
+client_secret = '44636a9d327c1e47aba28a9b50a22b39ac4caeb4'
+
+root_endpoint = 'https://api.github.com'
+
+headers = {'Accept' : 'application/vnd.github.v3+json', 'User-Agent' : 'stamaimer'}
+
+
+
+mongo_client = pymongo.MongoClient('mongodb://username:pwd@127.0.0.1:27017/msif')
+
+msif = mongo_client.msif
+
+github_users = msif.github_users
+
+
+
+endpoint = root_endpoint + '/users?since=%d'
+
+
+
+ratelimit_remaining = '5000'
+
+ratelimit_reset = time.time()
+
+
+
+def set_ratelimit_info(headers):
+
+    global ratelimit_remaining
+
+    ratelimit_remaining = headers['X-RateLimit-Remaining']
+
+    global ratelimit_reset
+
+    ratelimit_reset = int(headers['X-RateLimit-Reset'])
+
+
+
+def retrieve(url):
+
+    while 1:
+
+        try:
+
+            print 'ratelimit_remaining : %s' % ratelimit_remaining
+
+            if '0' == ratelimit_remaining:
+
+                print 'sleeping %f seconds...' % (ratelimit_reset - time.time())
+
+                time.sleep(ratelimit_reset - time.time())
+
+            print 'request : %s' % url
+
+            response = requests.get(url, params = {'client_id' : client_id, 'client_secret' : client_secret}, headers = headers)
+
+            if 200 == response.status_code:
+
+                print 'request : %s success' % url
+
+                return response
+
+            else:
+
+                set_ratelimit_info(response.headers)
+
+                print 'request : %s %d' % (url, response.status_code)
+
+                print json.dumps(response.json(), indent = 4)
+
+                if 404 == response.status_code:
+
+                    return None
+
+        except requests.exceptions.ConnectionError:
+
+            print 'requests.exceptions.ConnectionError'
+
+            last_insert_id = github_users.find().limit(1).sort([('$natural', -1)])[0]['id']
+
+            url = endpoint + '?since=%d' % last_insert_id
+
+            get_all_users(url)
+
+
+
+def get_single_user(url):
+
+    response = retrieve(url)
+
+    if response:
+
+        set_ratelimit_info(response.headers)
+
+        user = response.json()
+
+        github_users.insert(user)
+
+        print user['login']
+
+
+
+def stuff(response):
+
+    next_url = response.links['next']['url']
+
+    set_ratelimit_info(response.headers)
+
+    users = response.json()
+
+    for user in users:
+
+        get_single_user(user['url'])
+
+    return next_url
+
+
+
+def get_all_users(url):
+
+    response = retrieve(url)
+
+    if response:
+
+        next_url = stuff(response)
+
+    while next_url:
+
+        response = retrieve(next_url)
+
+        if response:
+
+            next_url = stuff(response)
+
+
+
+if __name__ == '__main__':
+
+    argument_parser = argparse.ArgumentParser(description='')
+
+    argument_parser.add_argument('-s', '--since', help='', type=int)
+
+    args = argument_parser.parse_args()
+
+    if args.since:
+
+        get_all_users(endpoint % args.since)
+
+    else:
+
+        get_all_users(endpoint % 0)
