8bd581ae1a7022cc45ed40dc8fa4c20878ab3802
curme <hiwiky@gmail.com>
2015-04-09 11:09:26 -0400

    add postagger

--- extraction/pos/postagger.py
+++ extraction/pos/postagger.py
@@ -1,3 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from nltk import pos_tag
+from nltk.tag.stanford import POSTagger, NERTagger
+from nltk.tokenize import word_tokenize
 
+
+if __name__ == "__main__":
+
+    sample = [
+        'these women taught English, they are now lying here!'
+    ]
+
+    tokens = word_tokenize(sample[0])
+    print pos_tag(tokens)
+    stagger= POSTagger('english-bidirectional-distsim.tagger',
+                       'stanford-postagger.jar')
+    print stagger.tag(tokens)
+    snerer = NERTagger('english.all.3class.distsim.crf.ser.gz',
+                       'stanford-ner.jar')
+    print snerer.tag(sample[0])
