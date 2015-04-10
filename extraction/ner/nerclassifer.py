7032c48a28c1768fab2426f8de37716de53fe927
curme <hiwiky@gmail.com>
2015-04-10 07:41:39 -0400

    nltk ner

--- extraction/ner/nerclassifer.py
+++ extraction/ner/nerclassifer.py
@@ -3,12 +3,14 @@
 from nltk.tokenize      import word_tokenize
 from nltk               import pos_tag
 from nltk.tag.stanford  import NERTagger
+from nltk               import ne_chunk
 
 s = 'my name is wisky!'
-token = word_tokenize(s)
-pos_t = pos_tag(token)
-print pos_t
-snerer = NERTagger('english.all.3class.distsim.crf.ser.gz',
-                   'stanford-ner.jar')
-print snerer.tag([pos_t])
+tokens = word_tokenize(s)
+pos_tg = pos_tag(tokens)
+ne     = ne_chunk(pos_tg, binary = True)
+print ne
+# snerer = NERTagger('english.all.3class.distsim.crf.ser.gz',
+#                    'stanford-ner.jar')
+# print snerer.tag([pos_t])
 
