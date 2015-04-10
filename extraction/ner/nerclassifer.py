99362237c3099b34239459df958eb449b1a9df92
curme <hiwiky@gmail.com>
2015-04-10 08:30:16 -0400

    modify sample

--- extraction/ner/nerclassifer.py
+++ extraction/ner/nerclassifer.py
@@ -5,11 +5,19 @@ from nltk               import pos_tag
 from nltk.tag.stanford  import NERTagger
 from nltk               import ne_chunk
 
-s = 'my name is wisky!'
-tokens = word_tokenize(s)
-pos_tg = pos_tag(tokens)
-ne     = ne_chunk(pos_tg, binary = True)
-print ne
+sample = [
+        u'Blog entry: summary of Weiwei Wang’s new PRL on magnon driven domain wall motion with DMI',
+        u'@IanBDunne Our kids are still excited about Barbie being pushed of the Van-der-Graaf generator from the pressure wave. Good job!',
+        u'#piday2015 works best in American date notation. Still nice, though.',
+        u'these women taught English, they are now lying here!'
+    ]
+
+for text in sample:
+    tokens = word_tokenize(text)
+    pos_tg = pos_tag(tokens)
+    ne     = ne_chunk(pos_tg, binary = True)
+    print ne
+
 # snerer = NERTagger('english.all.3class.distsim.crf.ser.gz',
 #                    'stanford-ner.jar')
 # print snerer.tag([pos_t])
