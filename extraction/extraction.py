95fcfa11a3cae35572305f59095cd42e634c396b
curme <hiwiky@gmail.com>
2015-04-13 08:50:18 -0400

    construct extractor

--- extraction/extraction.py
+++ extraction/extraction.py
@@ -3,9 +3,36 @@
 import json
 from nltk               import pos_tag
 from nltk               import ne_chunk
-from nltk.stem.wordnet  import WordNetLemmatizer
-from util.tokenizer_ark import tokenizeRawTweetText
+from nltk.stem.wordnet  import WordNetLemmatizer    as Lemmatizer
 from util.mitie         import named_entity_extractor
+from util.tokenizer_ark import tokenizeRawTweetText as tokenizer
+
+# extractor for short text, such as twitter and facebook
+def extractor(peer_text):
+    lemmatizer= Lemmatizer()
+    entities  = []
+
+    # redundant entities infusion will implement latter
+
+    # tokenization
+    for item in peer_text:
+        content = item['content']
+
+        # tokenization
+        tokens  = tokenizer(content)
+        # normalization
+        for i in range(len(tokens)): tokens[i] = lemmatizer.lemmatize(tokens[i])
+
+        item['tokens'] = tokens
+
+    # pos tagging
+    for item in peer_text:
+        tokens  = item['tokens']
+        tokens_p= pos_tag(tokens)
+
+        item['pos'] = tokens_p
+
+    print peer_text
 
 if __name__ == "__main__":
 
@@ -14,5 +41,7 @@ if __name__ == "__main__":
     with file('status.json', 'r') as f:
         status = json.load(f)
 
-    print status
-
+    for peer in status:
+        name     = peer.keys()[0] 
+        print name, '-'*50
+        entities = extractor(peer[name])
