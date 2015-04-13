666244ae412aca9e82edbda393a4573d7965d0f9
curme <hiwiky@gmail.com>
2015-04-13 07:36:06 -0400

    modify extraction

--- extraction/extraction.py
+++ extraction/extraction.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import json
+from nltk               import pos_tag
+from nltk               import ne_chunk
+from nltk.stem.wordnet  import WordNetLemmatizer
+from util.tokenizer_ark import tokenizeRawTweetText
+from util.mitie         import named_entity_extractor
+
+if __name__ == "__main__":
+
+    with file('tweets.json', 'r') as f:
+        tweets = json.load(f)
+    with file('status.json', 'r') as f:
+        status = json.load(f)
+
+    print status
 
