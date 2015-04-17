488101e354c637644fdd2e586eb855c26f50900f
curme <hiwiky@gmail.com>
2015-04-17 01:41:55 -0400

    auto crawl

--- twitter/collector_by_web.py
+++ twitter/collector_by_web.py
@@ -8,37 +8,39 @@ from lxml       import cssselect
 
 host    = "https://mobile.twitter.com"
 peers   = [
-    {'name':'Hans Fangoh',  'link':'/ProfCompMod?p=s',  'role':'professor'},
-    {'name':'Tiffany Horan','link':'/TiffanyHoran?p=i', 'role':'artist'},
-    {'name':'Darius H',     'link':'/ComethTheNerd?p=s','role':'ms engineer'},
-    {'name':'Danny mc',     'link':'/Danny_Mc12?p=s',   'role':'c# coder'},
-    {'name':'Jiri Mocicka', 'link':'/givision?p=s',     'role':'DesignDirector'},
-    {'name':'R Fouchaux',   'link':'/thefooshshow?p=s', 'role':'web teacher'},
-    {'name':'Ev Williams',  'link':'/ev?p=s',           'role':'ios app owner'},
-    {'name':'Ryan Seacrest','link':'/RyanSeacrest?p=s', 'role':'famous host'},
-    {'name':'Jenna Lucas',  'link':'/JennaLucas81?p=s', 'role':'primary tcher'},
-    {'name':'Adam Lofting', 'link':'/adamlofting?p=s',  'role':'a mozilla lead'}
+    {'name':'Hans Fangoh',  'link':'/ProfCompMod',  'username':'@ProfCompMod'},
+    {'name':'Tiffany Horan','link':'/TiffanyHoran', 'username':'@TiffanyHoran'},
+    {'name':'Darius H',     'link':'/ComethTheNerd','username':'@ComethTheNerd'},
+    {'name':'Danny mc',     'link':'/Danny_Mc12',   'username':'@Danny_Mc12'},
+    {'name':'Jiri Mocicka', 'link':'/givision',     'username':'@givision'},
+    {'name':'R Fouchaux',   'link':'/thefooshshow', 'username':'@thefooshshow'},
+    {'name':'Ev Williams',  'link':'/ev',           'username':'@ev'},
+    {'name':'Ryan Seacrest','link':'/RyanSeacrest', 'username':'@RyanSeacrest'},
+    {'name':'Jenna Lucas',  'link':'/JennaLucas81', 'username':'@JennaLucas81'},
+    {'name':'Adam Lofting', 'link':'/adamlofting',  'username':'@adamlofting'}
 ]
-
 
 # tweets json format:
 # {
 #   "name"      :   name,
+#   "username"  :   username,
 #   "time"      :   time,
+#   "link"      :   link,
 #   "tweets"    :   [{'tweet': tw1, 'time': ti1}, 
 #                    {'tweet': tw2, 'time': ti2}]
 # }
 def get_tweets(peer):
     name = peer['name']
+    unam = peer['username']
     link = peer['link']
-    role = peer['role']
     time = str(date.today())
     t_cons_xpath = '//*[@id="main_content"]/div[2]/table'
-    tweets = {"name":name, "time":time, "tweets": []}
+    tweets = {"name":name, "username":unam, "time":time, 
+              "link":link, "tweets": []}
     page_count = 1
 
     web_page = requests.get( host + link )
-    print "STAT: Crawl start."
+    print "STAT: Crawl tweets start."
 
     while True:
         print "STAT: Start page %d tweets crawl. link : %s" % (page_count, link)
@@ -59,8 +61,64 @@ def get_tweets(peer):
         except:
             break
 
-    print "SUCC: Crawl done."
+    print "SUCC: Crawl tweets done."
     return tweets
+
+def get_followers(peer, filter = False):
+    name = peer['name']
+    link = peer['link']
+    fers = []
+    page_count = 1
+
+    web_page = requests.get( host + link )
+    tree     = html.fromstring(web_page.text.encode('utf8'))
+    fer_butt = tree.cssselect('td.stat')[2]
+    fer_link = fer_butt.cssselect('a')[0].get('href')
+    web_page = requests.get( host + fer_link )
+    print "STAT: Crawl followers start."
+
+    while True:
+        print "STAT: Start page %d followers crawl." % page_count
+        tree     = html.fromstring(web_page.text.encode('utf8'))
+        foll_line= tree.cssselect('div.user-list')[0]
+        f_cons   = foll_line.cssselect('table.user-item')
+
+        for con in f_cons:
+            f_fullname  = con.cssselect('strong.fullname')[0].text_content()
+            f_username  = con.cssselect('span.username')[0].text_content()
+            f_link      = con.cssselect('td.screenname a')[1].get("href")
+            fers.append({'name':f_fullname, 'username':f_username,
+                         'link':f_link})
+        try:
+            # refresh link
+            link     = foll_line.cssselect('div.w-button-more a')[0].get("href")
+            # refresh web_page
+            web_page = requests.get( host + link )
+            page_count += 1
+        except:
+            break
+
+    if filter:
+        print "WARN: Filter flag opened."
+        print "STAT: Filter start. Kick out users whose tweets less than 500."
+
+        for follower in fers:
+            f_name = follower['name']
+            f_link = follower['link']
+            f_page = requests.get( host + f_link )
+            f_tree = html.fromstring(f_page.text.encode('utf8'))
+            f_twee = f_tree.cssselect('td.stat')[0]
+            f_tnum = f_twee.cssselect('div.statnum')[0].text_content()
+            f_tnum = int("".join(f_tnum.split(',')))
+
+            if f_tnum < 500:
+                fers.remove(follower)
+                print "WARN: %s kicked out." % f_name.encode('utf8')
+
+        print "SUCC: Filter done."
+
+    print "SUCC: Crawl followers done."
+    return fers
 
 def timer(raw_time):
 
@@ -108,10 +166,10 @@ def timer(raw_time):
         return raw_time
 
 def check_redundant(client, peer):
-    name = peer['name']
-    tweets = client.mruirf.twitter_tweets
+    username = peer['username']
+    tweets   = client.mruirf.twitter_tweets
 
-    if tweets.find_one({"name":name}):
+    if tweets.find_one({"username":username}):
         print "WARN: Already in database."
         return True
     else:
@@ -124,16 +182,24 @@ def store(client, tweets_json):
     tweets.insert(tweets_json)
     print "SUCC: Stored."
 
+
 if __name__ == '__main__':
 
     client = MongoClient('mongodb://localhost:27017/')
 
-    tweets = client.mruirf.twitter_tweets
-
     for peer in peers:
         print peer['name'], "-"*50
 
+        # get tweets
         if False == check_redundant(client, peer):
             store(client, get_tweets(peer))
+
+        # get followers
+        followers = get_followers(peer, True)
+        for follower in followers:
+            peers.append(follower)
+
+        # get an 100 p corpus
+        if client.mruirf.twitter_tweets.count() > 100 : break
 
         print
