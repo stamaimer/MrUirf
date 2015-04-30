b0f3787eda570c80129914a0ead3633b95bab868
curme <hiwiky@gmail.com>
2015-04-29 22:37:11 -0400

    set filter limit

--- extraction/util/relextractor_map.py
+++ extraction/util/relextractor_map.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
 
+'''
+    MEMO
+    1. 2015-4-30
+    In the first test, we choose the peers crawled in 2015-04-23, totally 100
+peers, as corpus. In this corpus, we induce more than 300,000 tweets and more
+than 60,000 patterns. So we want to filter out a set of patterns, which peers
+using most and the size of the set will not be too big. After evaluation, we
+found that those patterns talked by more than 40 peers are frequently used, and
+the tweets in these patterns make up nearly 60 percent. Finally we determined
+use this set.
+'''
+
 import re
 from pymongo     import MongoClient
 
@@ -80,36 +92,38 @@ def pos_sentence_collection(db, filter):
         print
         print
 
-def evaluate(db):
+def evaluate(db, people_limit):
 
     sents   = db.twitter_sentences
     patterns= sents.find()
     result  = []
 
     for pattern in patterns:
-
         item = {}
         item['pattern'] = pattern['pattern']
         item['people'] = []
         item['sentences'] = len(pattern['set'])
-
         for set in pattern['set']:
-
             if set['username'] not in item['people']:
-
                 item['people'].append(set['username'])
-
         item['people'] = len(item['people'])
+
         result.append(item)
 
-    with file('output.txt', 'w') as f:
+    all = sum([item['sentences'] for item in result])
+    filter = []
+    for item in result:
+        if item['people'] >= people_limit:
+            filter.append(item)
+    fter= sum([item['sentences'] for item in filter])
+    print "fter in all: %s %%" % str(100 * float(fter) / all)
+    print "patterns: %d" % len(filter)
 
+    with file('frequent_patterns.txt', 'w') as f:
         for item in result:
-
-            if item['people'] >= 5:
-
+            if item['people'] >= people_limit:
                 f.write('%s\t%s\t%s\n' % (item['pattern'], item['people'], \
-                                      item['sentences']))
+                        item['sentences']))
 
 
 if __name__ == "__main__":
@@ -117,6 +131,10 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://localhost:27017/')
     twdb   = client.msif
 
+    # pos_sentence_collection(twdb, {'time':'2015-04-23'})
+    evaluate(twdb, 40)
+
+    ''' to fix cursor timeout
     twsents= twdb.twitter_sentences
     tweets = twdb.twitter_tweets
     people = []
@@ -137,6 +155,5 @@ if __name__ == "__main__":
 
     for peer in people_not:
         pos_sentence_collection(twdb, {'username':peer})
+    '''
 
-    #pos_sentence_collection(twdb, {'time':'2015-04-23'})
-    #evaluate(twdb)
